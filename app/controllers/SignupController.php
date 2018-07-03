9c552474ff3fb3608420ee3c5c5baae119edff2b
mostafajafari68 <mjafari632@gmail.com>
2018-07-03 15:10:11 +0330

    project structure and config htaccess file

---
+++ app/controllers/SignupController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 02/07/2018
+ * Time: 04:03 PM
+ */
+
+class SignupController
+{
+
+}
---
+++ app/models/Users.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 02/07/2018
+ * Time: 04:07 PM
+ */
+
+class Users
+{
+
+}
